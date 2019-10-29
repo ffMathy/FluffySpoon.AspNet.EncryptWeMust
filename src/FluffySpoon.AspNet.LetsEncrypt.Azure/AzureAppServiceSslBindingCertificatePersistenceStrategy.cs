9ae6c77a3e52afbeb8d5ea87d6356bf0cf5b44e3
Simon Smith <simon.smith@freestyle.agency>
2019-10-29 17:28:12 +0000

    Changes to use 'var' for consistency

--- src/FluffySpoon.AspNet.LetsEncrypt.Azure/AzureAppServiceSslBindingCertificatePersistenceStrategy.cs
+++ src/FluffySpoon.AspNet.LetsEncrypt.Azure/AzureAppServiceSslBindingCertificatePersistenceStrategy.cs
@@ -328,12 +328,12 @@ namespace FluffySpoon.LetsEncrypt.Azure
 		{
 			var azureCert = await GetExistingAzureCertificateAsync(persistenceType);
 
-			X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+			var certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
 			certStore.Open(OpenFlags.ReadOnly);
 
 			try
 			{
-				X509Certificate2Collection certCollection = certStore.Certificates.Find(
+				var certCollection = certStore.Certificates.Find(
 											X509FindType.FindByThumbprint,
 											// Replace below with your certificate's thumbprint
 											azureCert.Thumbprint,
@@ -342,7 +342,7 @@ namespace FluffySpoon.LetsEncrypt.Azure
 				// Get the first cert with the thumbprint
 				if (certCollection.Count > 0)
 				{
-					X509Certificate2 cert = certCollection[0];
+					var cert = certCollection[0];
 					return cert;
 				}
 			}
