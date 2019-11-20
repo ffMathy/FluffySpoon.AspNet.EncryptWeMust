e7a7c66c2a0c2054587194199c6983e7a328827e
Simon Smith <simon.smith@freestyle.agency>
2019-11-20 10:20:47 +0000

    Improved readability of DistributedCacheChallengePersistenceStrategy by implementing IChallengePersistenceStrategy instead of deriving from CustomChallengePersistenceStrategy Improved deletion in DistributedCacheChallengePersistenceStrategy

--- src/FluffySpoon.AspNet.LetsEncrypt.Redis/DistributedCacheChallengePersistenceStrategy.cs
+++ src/FluffySpoon.AspNet.LetsEncrypt.Redis/DistributedCacheChallengePersistenceStrategy.cs
@@ -11,39 +11,60 @@ using Newtonsoft.Json;
 
 namespace FluffySpoon.AspNet.LetsEncrypt.Redis
 {
-	public class DistributedCacheChallengePersistenceStrategy : CustomChallengePersistenceStrategy
+	public class DistributedCacheChallengePersistenceStrategy : IChallengePersistenceStrategy
 	{
 		private const string Key = "FluffySpoon_Challenges";
+
+		private readonly ILogger<DistributedCacheChallengePersistenceStrategy> _logger;
+		private readonly IDistributedCache _cache;
+		private readonly TimeSpan _expiry;
 
 		public DistributedCacheChallengePersistenceStrategy(
 			ILogger<DistributedCacheChallengePersistenceStrategy> logger,
 			IDistributedCache cache, 
-			TimeSpan expiry) : base(
-				new ChallengeType[] { ChallengeType.Http01 },
-				async (challenges) =>
-				{
-					var json = challenges == null ? null : JsonConvert.SerializeObject(challenges.ToArray());
-					logger.LogDebug("Persisting challenges {0}", json);
-
-					var bytes = json == null ? null : Encoding.UTF8.GetBytes(json);
-
-					await cache.SetAsync(Key, bytes, new DistributedCacheEntryOptions()
-					{
-						AbsoluteExpirationRelativeToNow = expiry
-					});
-				},
-				async () => {
-					var bytes = await cache.GetAsync(Key);
-					var json = Encoding.UTF8.GetString(bytes);
-					var challenges = JsonConvert.DeserializeObject<IEnumerable<ChallengeDto>>(json);
-
-					return challenges;
-				},
-				async (challenges) =>
-				{
-					await cache.RemoveAsync(Key);
-				})
+			TimeSpan expiry)
 		{
+			_logger = logger;
+			_cache = cache;
+			_expiry = expiry;
+		}
+
+		public bool CanHandleChallengeType(ChallengeType challengeType)
+		{
+			return challengeType == ChallengeType.Http01;
+		}
+
+		public async Task DeleteAsync(IEnumerable<ChallengeDto> challenges)
+		{
+			var persistedChallenges = await RetrieveAsync();
+			var challengesToPersist = persistedChallenges
+				.Where(x =>
+					!challenges.Any(y => y.Token == x.Token))
+				.ToList();
+
+			await PersistAsync(challengesToPersist);
+		}
+
+		public async Task PersistAsync(IEnumerable<ChallengeDto> challenges)
+		{
+			var json = challenges == null ? null : JsonConvert.SerializeObject(challenges.ToArray());
+			_logger.LogDebug("Persisting challenges {0}", json);
+
+			var bytes = json == null ? null : Encoding.UTF8.GetBytes(json);
+
+			await _cache.SetAsync(Key, bytes, new DistributedCacheEntryOptions()
+			{
+				AbsoluteExpirationRelativeToNow = _expiry
+			});
+		}
+
+		public async Task<IEnumerable<ChallengeDto>> RetrieveAsync()
+		{
+			var bytes = await _cache.GetAsync(Key);
+			var json = Encoding.UTF8.GetString(bytes);
+			var challenges = JsonConvert.DeserializeObject<IEnumerable<ChallengeDto>>(json);
+
+			return challenges;
 		}
 	}
 }
