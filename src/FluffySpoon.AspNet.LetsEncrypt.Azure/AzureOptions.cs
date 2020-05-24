f5008726dc37cb57d517797cfdb72859fad8ca77
Will Dean <github_will@indcomp.co.uk>
2020-05-24 11:02:15 +0100

    New update mechanism for Azure certs (#78)
    * Fixes problem with trying to update certs which are in use
    * Transparently treats slots the same way as normal apps
    * Fixes problem with base app settings being changed rather than slot settings
    * Refactors very large PersistAsync method into parts

--- src/FluffySpoon.AspNet.LetsEncrypt.Azure/AzureOptions.cs
+++ src/FluffySpoon.AspNet.LetsEncrypt.Azure/AzureOptions.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+﻿using System;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 
 namespace FluffySpoon.AspNet.LetsEncrypt.Azure
 {
 	public class AzureOptions
 	{
 		public string ResourceGroupName { get; set; }
-		public string Slot { get;set; }
 
 		public AzureCredentials Credentials { get; set; }
+		
+		[Obsolete("This is no longer necessary, all matching apps & slots are automatically included")]
+		public string Slot { get;set; }
 	}
 }
