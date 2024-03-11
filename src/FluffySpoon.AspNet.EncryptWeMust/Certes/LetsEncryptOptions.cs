6f6ac68cad178abadb5838edc8b29274d72d3519
fntc <sebkuemmel@gmail.com>
2024-03-11 17:03:32 +0100

    add preferred chain option to configuration (#228)
    Co-authored-by: Sebastian Kuemmel <sebastian.kuemmel@taptill.de>

--- src/FluffySpoon.AspNet.EncryptWeMust/Certes/LetsEncryptOptions.cs
+++ src/FluffySpoon.AspNet.EncryptWeMust/Certes/LetsEncryptOptions.cs
@@ -52,6 +52,12 @@ namespace FluffySpoon.AspNet.EncryptWeMust.Certes
 		public KeyAlgorithm KeyAlgorithm { get; set; } = KeyAlgorithm.ES256;
 
 		/// <summary>
+		/// Preferred Chain, when set will define up to which certificate the 
+		/// LetsEncrypt Long Chain: ISRG Root X1
+		/// </summary>
+		public string PreferredChain { get; set; } = "ISRG Root X1";
+
+		/// <summary>
 		/// Get or set a delay before the initial run of the renewal service (subsequent runs will be at 1hr intervals)
 		/// On some platform/deployment systems (e.g Azure Slot Swap) we do not want the renewal service to start immediately, because we may not
 		/// yet have incoming requests (e.g. for challenges) directed to us. 
